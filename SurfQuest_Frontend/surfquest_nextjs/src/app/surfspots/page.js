fb07d34207e9b53e2715aaa8e313c908e97ff79d
HolbieWan <8841@holbertonstudents.com>
2025-01-25 23:34:52 +0100

    Update: app/surfspots/page.js with text card styled in white - looks good

--- SurfQuest_Frontend/surfquest_nextjs/src/app/surfspots/page.js
+++ SurfQuest_Frontend/surfquest_nextjs/src/app/surfspots/page.js
@@ -64,7 +64,7 @@ export default function SurfSpotsPage() {
   return (
     <div className="flex flex-col items-center justify-start pt-20 h-screen bg-black text-white">
       <h1 className="text-4xl font-bold">Surf spots</h1>
-      <p className="text-lg mt-4">Select Surf-Zone</p>
+      <p className="text-lg mt-8">Select Surf-Zone</p>
       <select
         className="mt-4 p-2 border border-gray-300 rounded bg-white text-black"
         value={selectedSurfZone}
@@ -83,22 +83,22 @@ export default function SurfSpotsPage() {
         <div className="grid grid-cols-1 p-4 gap-8 rounded-md">
           {filteredSurfSpots.map((surfspot, index) => (
             <div key={index} className="flex flex-col md:flex-row items-start justify-center space-y-4 md:space-y-0 md:space-x-4">
-              <div className="bg-black rounded-md overflow-hidden group flex-shrink-0 w-full md:w-1/2 lg:w-1/3" style={{ height: '400px' }}>
+              <div className="bg-black rounded-md overflow-hidden group flex-shrink-0 w-full md:w-1/2 lg:w-2/4" style={{ height: '400px' }}>
                 {surfspot.spot_images && surfspot.spot_images.map((image, imgIndex) => (
                   <img key={imgIndex} src={image.image} alt={surfspot.name} className="w-full h-full object-cover rounded-md transform transition-transform duration-500 group-hover:scale-110"/>
                 ))}
               </div>
-              <div className="bg-black rounded-md p-4 flex flex-col justify-center border border-white overflow-hidden w-full md:w-1/2" style={{ height: '400px' }}>
-                <h2 className="text-white text-xl font-bold text-center md:text-left">{surfspot.name}</h2>
-                <div className="mt-2 text-sm text-white text-center md:text-left font-semibold">{surfspot.surfzone.name}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">{surfspot.description}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Best months: {surfspot.best_months.join(', ')}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Best swell direction: {surfspot.best_swell_direction}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Best swell size: {surfspot.best_swell_size} ft</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Best tide: {surfspot.best_tide.join(', ')}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Best wind direction: {surfspot.best_wind_direction}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Surf hazards: {surfspot.surf_hazards.join(', ')}</div>
-                <div className="mt-2 text-sm text-white text-center md:text-left">Surf level: {surfspot.surf_level.join(', ')}</div>
+              <div className="bg-white rounded-md p-4 flex flex-col justify-center border border-gray-300 overflow-hidden w-full md:w-1/2 lg:w-1/3" style={{ height: '400px' }}>
+                <h2 className="text-black text-2xl font-bold text-center md:text-left">{surfspot.name}</h2>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left font-semibold">{surfspot.surfzone.name}</div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">{surfspot.description}</div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Best months: <span className="text-black font-bold">{surfspot.best_months.join(', ')}</span></div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Best swell direction: <span className="text-black font-bold">{surfspot.best_swell_direction}</span></div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Best swell size: <span className="text-black font-bold">{surfspot.best_swell_size} ft</span></div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Best tide: <span className="text-black font-bold">{surfspot.best_tide.join(', ')}</span></div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Best wind direction: <span className="text-black font-bold">{surfspot.best_wind_direction}</span></div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Surf hazards: <span className="text-black font-bold">{surfspot.surf_hazards.join(', ')}</span></div>
+                <div className="mt-2 text-sm text-gray-700 text-center md:text-left">Surf level: <span className="text-black font-bold">{surfspot.surf_level.join(', ')}</span></div>
               </div>
             </div>
           ))}
