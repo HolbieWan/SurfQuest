189d352fc040d71434ec6e14fd6b1ce489ca3552
HolbieWan <8841@holbertonstudents.com>
2025-01-26 23:48:22 +0100

    Update: components/SurfZones.js: fix homepage to display best surfing destination text only if the api sends a response

--- SurfQuest_Frontend/surfquest_nextjs/src/app/components/SurfZones.js
+++ SurfQuest_Frontend/surfquest_nextjs/src/app/components/SurfZones.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
+import Link from 'next/link';
 
 
 const surfZonesApiUrl = 'http://localhost:8000/api/surfzones/';
@@ -65,21 +66,24 @@ export default function SurfZonesRoute() {
 
   return (
     <div className="flex flex-col items-center justify-start pt-16 bg-black text-white">
-      <h2 className="text-2xl font-bold mb-6 text-center">Best surfing destinations in { currentMonth }</h2>
+      {responseData && <h2 className="text-2xl font-bold mb-6 text-center">Best surfing destinations in {currentMonth}</h2>}
       {error && <div><p className="text-red-500 text-sm">{error}</p></div>}
       {loading && <div><p className="text-blue-500 text-sm">Loading...</p></div>}
       {responseData && (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-4 gap-4 rounded-md">
           {responseData.map((surfzone, index) => (
-              <div key={index} className="bg-black rounded-md p-4 relative overflow-hidden group">
-                {surfzone.zone_images && surfzone.zone_images.map((image, imgIndex) => (
-                <img key={imgIndex} src={image.image} alt={surfzone.name} className="inset-0 mt-4 w-full h-64 object-cover rounded-md transform transition-transform duration-500 group-hover:scale-110"/>
-                ))}
-                <div className="absolute inset-0 bg-black bg-opacity-10 flex flex-col justify-center p-4">
-                  <h2 className="text-white text-2xl font-bold text-center text-shadow-xl" >{surfzone.name}</h2>
-                  <div className="mt-2 text-md text-white text-center font-semibold text-shadow-xl">{surfzone.country.name}</div>
-                </div>
+            <div key={index} className="bg-black rounded-md p-4 relative overflow-hidden group">
+              <Link href={`/surfspots?surfzone=${surfzone.name}`} legacyBehavior>
+                <a className="absolute inset-0 z-10"></a>
+              </Link>
+              {surfzone.zone_images && surfzone.zone_images.map((image, imgIndex) => (
+              <img key={imgIndex} src={image.image} alt={surfzone.name} className="inset-0 mt-4 w-full h-64 object-cover rounded-md transform transition-transform duration-500 group-hover:scale-110"/>
+              ))}
+              <div className="absolute inset-0 bg-black bg-opacity-10 flex flex-col justify-center p-4">
+                <h2 className="text-white text-2xl font-bold text-center text-shadow-xl" >{surfzone.name}</h2>
+                <div className="mt-2 text-md text-white text-center font-semibold text-shadow-xl">{surfzone.country.name}</div>
               </div>
+            </div>
           ))}
         </div>
       )}
